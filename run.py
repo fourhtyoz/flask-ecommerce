5789a54ef3e72bacfed85e3eb61b758459b1d105
Fourhtyoz <59650145+fourhtyoz@users.noreply.github.com>
2022-09-05 09:32:36 +0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,10 @@
+from market import app
+
+# Checks if the run.py file has executed directly and not imported
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# venv (cmd):
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+# . venv/bin/activate 
